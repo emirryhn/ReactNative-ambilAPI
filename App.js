4d48178fbbabd07f9205a8bb25c92c6df539fb12
Emir Rayhan <emir123.er@gmail.com>
2021-09-27 11:24:13 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-dom';
+// import { StyleSheet, Text,View, SafeAreaView, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App=()=>{
+      fetch('https://edifiapi.enseval.com/edifi/testimoni')
+      .then(function(resp){
+        return resp.text();
+      })
+      .then(function(data){
+        let parser = new Parser(),
+        xmlDoc = parser.parseFromString(data,'text.xml');
+        console.log(xmlDoc.getElementByTagName('Gender'));
+      })
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    export default App;
+// const styles = StyleSheet.create({
+//   button:{
+//     position: 'absolute',
+//     top: 220,
+//     left: 200
+//   },
+//   container: {
+
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+    
+//   },
+// });
